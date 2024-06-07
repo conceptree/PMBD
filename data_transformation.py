4d19a93b5e6f7a3fcbf23745dc64bbe942aba237
conceptree <nunomtrodrigues@gmail.com>
2024-06-07 17:52:41 +0100

    outliers for temp

--- data_transformation.py
+++ data_transformation.py
@@ -1,13 +1,10 @@
 import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, round, floor, mean as _mean, stddev as _stddev
 
 # Definir a mesma versão do Python para driver e worker
 os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3.11'
 os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/local/bin/python3.11'
-
-# ----------------------------------------------------------------------------------------------------------------- #
-
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, round, floor
 
 # Inicializar a Spark Session
 spark = SparkSession.builder \
@@ -36,6 +33,40 @@ weather_df_with_location = weather_df_with_location \
     .withColumn("temp_max", col("TMAX") / 10) \
     .withColumn("temp_min", col("TMIN") / 10)
 
+# Método IQR para detectar outliers
+def calcular_limites_iqr(df, coluna):
+    quantiles = df.approxQuantile(coluna, [0.25, 0.75], 0.0)
+    q1, q3 = quantiles
+    iqr = q3 - q1
+    limite_inferior = q1 - 1.5 * iqr
+    limite_superior = q3 + 1.5 * iqr
+    return limite_inferior, limite_superior
+
+temp_max_inferior, temp_max_superior = calcular_limites_iqr(weather_df_with_location, "temp_max")
+temp_min_inferior, temp_min_superior = calcular_limites_iqr(weather_df_with_location, "temp_min")
+
+weather_df_with_location = weather_df_with_location.filter(
+    (col("temp_max") >= temp_max_inferior) & (col("temp_max") <= temp_max_superior) &
+    (col("temp_min") >= temp_min_inferior) & (col("temp_min") <= temp_min_superior)
+)
+
+# Método Z-score para detectar outliers
+def calcular_limites_z_score(df, coluna):
+    stats = df.select(_mean(col(coluna)).alias('mean'), _stddev(col(coluna)).alias('stddev')).collect()
+    mean = stats[0]['mean']
+    stddev = stats[0]['stddev']
+    limite_inferior = mean - 3 * stddev
+    limite_superior = mean + 3 * stddev
+    return limite_inferior, limite_superior
+
+temp_max_inferior, temp_max_superior = calcular_limites_z_score(weather_df_with_location, "temp_max")
+temp_min_inferior, temp_min_superior = calcular_limites_z_score(weather_df_with_location, "temp_min")
+
+weather_df_with_location = weather_df_with_location.filter(
+    (col("temp_max") >= temp_max_inferior) & (col("temp_max") <= temp_max_superior) &
+    (col("temp_min") >= temp_min_inferior) & (col("temp_min") <= temp_min_superior)
+)
+
 # Arredondar a elevação para o número inteiro mais próximo
 weather_df_with_location = weather_df_with_location.withColumn("Elevation", floor(round(col("Elevation"))))
 
@@ -43,7 +74,7 @@ weather_df_with_location = weather_df_with_location.withColumn("Elevation", floo
 weather_df_with_location = weather_df_with_location.withColumnRenamed("EVAP", "Evaporation") \
                                                    .withColumnRenamed("PRCP", "Precipitation")
 
-# Filtrar entradas com null em Precipitation, maxTempCelsius ou minTempCelsius
+# Filtrar entradas com null em Precipitation, temp_max ou temp_min
 weather_df_with_location = weather_df_with_location.filter(
     col("Precipitation").isNotNull() &
     col("temp_max").isNotNull() &
@@ -64,5 +95,3 @@ print(f"Dataset final salvo em {output_path}")
 
 # Encerrar a Spark Session
 spark.stop()
-
-
